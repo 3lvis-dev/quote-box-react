a3c06c76f1fe462cd8445c094859541ebae6c5a6
3lvis-dev <ea.espinoza.f@gmail.com>
2022-07-23 15:30:21 -0400

    Mini project with react and vite quote box

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import users from './json/users.json'
+import QuoteBox from './components/QuoteBox'
+import colors from './utils/colors'
+
+function App() {
+
+  const getRandomElement = arr => {
+    const indexRandom = Math.floor(Math.random() * arr.length)
+    return arr[indexRandom]
+  }
+
+  let quoteRandom = getRandomElement(users)
+  let colorRandom = getRandomElement(colors)
+
+  const [randomQuote, setRandomQuote] = useState(quoteRandom)
+  const [randomColors, setRandomColors] = useState(colorRandom)
+
+  const objStyle = {
+    backgroundColor: randomColors
+  }
+
+  const getRandomAll = () => {
+    quoteRandom = getRandomElement(users)
+    colorRandom = getRandomElement(colors)
+
+    setRandomQuote(quoteRandom)
+    setRandomColors(colorRandom)
+  }
+
+  return (
+    <div id='app' className="App" style={objStyle}>
+      <QuoteBox 
+        randomQuote={randomQuote} 
+        randomColors={randomColors}
+        getRandomAll={getRandomAll}
+      />
+      <br />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/QuoteBox.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const QuoteBox = ({randomQuote, randomColors, getRandomAll}) => {
+
+  console.log(randomColors)
+
+  const objectStyle = {
+    color: randomColors
+  }
+
+  const objectBgStyle = {
+    backgroundColor: randomColors
+  }
+
+  return (
+    <article className='card' style={objectStyle}>
+      <div className='card__container'>
+        <div className='card__quote'>
+          <div className='card__quote--icon'><strong>&quot; </strong></div>
+          <div className='card__quote--item'>
+            <p> {randomQuote.quote}</p>
+          </div>
+        </div>
+        <div className='card__author'>
+          <p>{randomQuote.author}</p>
+          <button onClick={getRandomAll} className='card__btn' style={objectBgStyle}>&#62;</button>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+export default QuoteBox
